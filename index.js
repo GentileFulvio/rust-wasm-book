d44ef3313a3c0026a8820e8405d6e1b66db797f3
Fulvio Gentile <fulvio.gentile@into.care>
2021-09-01 19:37:21 +0200

    chapter 1.2

--- index.js
+++ index.js
@@ -2,7 +2,3 @@
 // webpack/webpack#6615, but in theory `import { greet } from './pkg';`
 // will work here one day as well!
 const rust = import('./pkg');
-
-rust
-  .then(m => m.greet('World!'))
-  .catch(console.error);
